fe9464cc018f1d09ffbbd8458860f70a07cfeeca
whisper <cndone@gmail.com>
2019-04-21 00:50:21 +0900

    #20190421 springboot,logback,springprofiles

---
+++ springboot-log/src/main/java/jp/whisper/log/SpringbootLogApplication.java
@@ -0,0 +1,21 @@
+package jp.whisper.log;
+
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import ch.qos.logback.classic.Logger;
+
+@SpringBootApplication
+public class SpringbootLogApplication {
+
+	public static void main(String[] args) {
+		Logger logger = (Logger) LoggerFactory.getLogger(SpringbootLogApplication.class);
+
+		logger.info("ログレベル [INFO] で出力されるログ");
+		logger.debug("ログレベル [DEBUG] で出力されるログ");
+
+		SpringApplication.run(SpringbootLogApplication.class, args);
+	}
+
+}
