49cc74e1864445b2eec8f39d2bbaf2e81640ca61
whisper <cndone@gmail.com>
2020-08-19 18:22:25 +0900

    #20200819 role hierarchy

--- springboot-security-db/src/main/java/jp/whisper/securitydb/config/SecurityConfig.java
+++ springboot-security-db/src/main/java/jp/whisper/securitydb/config/SecurityConfig.java
@@ -4,11 +4,16 @@ import jp.whisper.securitydb.service.UserSercice;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchy;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.Collection;
 
 /**
  * @ClassName SecurityConfig
@@ -44,5 +49,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    RoleHierarchy roleHierarchy(){
+        RoleHierarchyImpl roleHierarchy = new RoleHierarchyImpl();
+        String hierarchy = "ROLE_dba > ROLE_admin \n ROLE_admin > ROLE_user"; //改行で区切る
+        roleHierarchy.setHierarchy(hierarchy);
+        return roleHierarchy;
     }
 }
